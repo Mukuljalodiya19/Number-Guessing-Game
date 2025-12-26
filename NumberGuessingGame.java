7e961c615db0e7447080bc3ca4741a6f248da132
Mukuljalodiya19 <mukuljalodiya@gmail.com>
2025-12-26 14:59:32 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,62 @@
+package project1;
+
+    import java.util.Scanner;
+import java.util.Random;
+
+    public class NumberGuessingGame {
+        public static void main(String[] args) {
+            Scanner kb = new Scanner(System.in);
+            Random random = new Random();
+
+            int lowerBound = 1;
+            int upperBound = 100;
+           int maxAttempts = 8;
+           int score = 0;
+           boolean playAgain = true;
+
+            System.out.println("-------Welcome to the Number Guessing Game!!!!-------");
+
+            while(playAgain){
+
+                int numberToGuess = random.nextInt(100)+1;
+                int attempts = 0;
+                boolean guessedCorrectly = false;
+
+                System.out.println("I have selected a number between "+ lowerBound + " and " + upperBound);
+                System.out.println("You have " + maxAttempts + " attempts to guess the number");
+
+                while(attempts < maxAttempts){
+                    System.out.println("Enter your guess number: ");
+                    int Guess = kb.nextInt();
+                    attempts++;
+
+                    if (Guess == numberToGuess){
+                        System.out.println("You Won!!!! You Guessed the number in " + attempts + " attempts.");
+                        score =  score+(maxAttempts - attempts + 1)*10;
+                        guessedCorrectly = true;
+                        break;
+                    } else if (Guess < numberToGuess) {
+                        System.out.println("Too low!!");
+                    }else {
+                        System.out.println("too high!!");
+                    }
+                }
+                if (!guessedCorrectly){
+                    System.out.println("You've used all attempts.");
+                    System.out.println("Sorry you failed to guess the number!!!");
+                    System.out.println("The correct number was: " + numberToGuess);
+                }
+                System.out.println("Current Score: "+score);
+
+                System.out.println("Do you want to play again ? (yes/no): ");
+                String choice = kb.next().toLowerCase();
+
+                if (!choice.equals("yes")) {
+                    playAgain = false;
+                }
+            }
+
+            System.out.println("Thank you for playing Game!!!!");
+            System.out.println("your Final Score :" + score);
+            }
+        }
